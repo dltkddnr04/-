8620a057d63d967d918b97d29be7c0eb2344d647
Sanguk Lee <dltkddnr04@icloud.com>
2022-06-17 09:17:02 +0900

    업데이트 관련코드 수정

--- twitch_auto_recorder.py
+++ twitch_auto_recorder.py
@@ -15,10 +15,6 @@ from PyQt5.QtCore import Qt
 client_id = "5ayor8kn22hxinl6way2j1ejzi41g2"
 client_secret = "8tp18ssnpzbrzyyf0he83q3lsfayyx"
 
-req = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials")
-json_data = json.loads(req.text)
-access_token = json_data["access_token"]
-
 # streamer_list.pikle 파일이 없으면 생성
 if not os.path.isfile("streamer_list.pickle"):
     with open("streamer_list.pickle", "wb") as f:
@@ -42,24 +38,25 @@ class MyApp(QWidget):
 
         self.start_program()
 
-        now_version = "1.0.0"
-        req = requests.get("https://api.github.com/repos/dltkddnr04/Twitch-Auto-Recorder/releases/latest")
-        last_version = req.json()["name"]
-
-        now_version = now_version.split('.')
-        last_version = last_version[1:].split('.')
-
-        if int(now_version[0]) != int(last_version[0]):
-            QMessageBox.information(self, "Error", "새로운 버전이 사용가능합니다.\n업데이트해주세요.\n업데이트 주소:\nhttps://github.com/dltkddnr04/Twitch-Auto-Recorder/releases")
-            sys.exit()
-        else:
-            if int(now_version[1]) != int(last_version[1]):
-                QMessageBox.information(self, "Error", "새로운 버전이 사용가능합니다.\n업데이트해주세요.\n업데이트 주소:\nhttps://github.com/dltkddnr04/Twitch-Auto-Recorder/releases")
-                sys.exit()
-            else:
-                if int(now_version[2]) != int(last_version[2]):
-                    QMessageBox.information(self, "Error", "새로운 버전이 사용가능합니다.\n업데이트해주세요.\n업데이트 주소:\nhttps://github.com/dltkddnr04/Twitch-Auto-Recorder/releases")
-                    sys.exit()
+        try:
+            req = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials")
+            json_data = json.loads(req.text)
+            access_token = json_data["access_token"]
+        except:
+            QMessageBox.information(self, "Error", "인터넷에 연결되어있지 않습니다.\n인터넷 연결을 확인해주세요.")
+            exit()
+        
+        try:
+            now_version = "1.0.0"
+            req = requests.get("https://api.github.com/repos/dltkddnr04/Twitch-Auto-Recorder/releases/latest")
+            json_data = json.loads(req.text)
+            latest_version = json_data["name"].replace("v", "")
+            if latest_version != now_version:
+                QMessageBox.information(self, "업데이트 알림", "새로운 버전이 사용가능합니다.\n업데이트해주세요.\n업데이트 주소:\nhttps://github.com/dltkddnr04/Twitch-Auto-Recorder/releases")
+                
+        
+        except:
+            QMessageBox.information(self, "Error", "인터넷에 연결되어있지 않습니다.\n인터넷 연결을 확인해주세요.")
 
     def createSetupGroup(self):
         groupbox = QGroupBox('설정')
@@ -68,7 +65,6 @@ class MyApp(QWidget):
         self.streamer_edit = qtwid.QLineEdit(self)
         self.save_btn = qtwid.QPushButton("추가",self)
         self.lbox_item = qtwid.QListWidget(self)
-        #self.btn_group = qtwid.QPushButton("등록해제",self)
 
         self.streamer_edit.setToolTip('스트리머의 영문 닉네임을 입력하세요')
         
@@ -89,19 +85,9 @@ class MyApp(QWidget):
         grid = QGridLayout()
 
         self.btn_remove = qtwid.QPushButton("등록해제",self)
-        #self.btn_start = qtwid.QPushButton("시작",self)
-        #self.btn_stop = qtwid.QPushButton("정지",self)
-
         grid.addWidget(self.btn_remove, 0, 0)
-        #grid.addWidget(self.btn_start, 1, 0)
-        #grid.addWidget(self.btn_stop, 2, 0)
-
         self.btn_remove.clicked.connect(self.Btn_removeClick)
-        #self.btn_start.clicked.connect(self.Btn_startClick)
-        #self.btn_stop.clicked.connect(self.Btn_stopClick)
-
         self.btn_remove.setEnabled(False)
-        #self.btn_stop.setEnabled(False)
 
         groupbox.setLayout(grid)
         return groupbox
@@ -224,31 +210,6 @@ class MyApp(QWidget):
 
         self.lbox_item.takeItem(self.lbox_item.currentRow())
         self.console_print(selected_streamer + "님 등록해제 완료")
-
-    """
-    def Btn_startClick(self):
-        self.btn_start.setEnabled(False)
-        # background_process thread 시작
-        threading.Thread(target=self.background_process, name="background_process").start()
-
-        self.console_print("프로세스 시작됨")
-        self.btn_stop.setEnabled(True)
-        return
-
-    def Btn_stopClick(self):
-        self.btn_stop.setEnabled(False)
-        # background_process thread 강제로 종료
-
-
-        self.console_print("프로세스 종료됨")
-        self.btn_start.setEnabled(True)
-        return
-
-    def background_process(self):
-        while True:
-            time.sleep(1)
-    """
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
